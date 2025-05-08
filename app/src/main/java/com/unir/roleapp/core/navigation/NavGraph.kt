c659e4ee36df8e1077ff3c0b1f1440bce7af6240
Pablo Camacho <pabloaseca@gmail.com>
2025-05-08 21:59:32 +0200

    Importacion de adventure desde develop-pablo

--- app/src/main/java/com/unir/roleapp/core/navigation/NavGraph.kt
+++ app/src/main/java/com/unir/roleapp/core/navigation/NavGraph.kt
@@ -27,6 +27,20 @@ import com.roleapp.core.di.LocalLanguageSetter
 import com.unir.roleapp.adventure.ui.screens.HomeAdventureScreen
 import com.unir.roleapp.core.navigation.LocalizedApp
 import com.unir.roleapp.home.ui.screens.HomeScreen
+import com.unir.roleapp.adventure.ui.screens.AdventureContextScreen
+import com.unir.roleapp.adventure.ui.screens.AdventureMainScreen
+import com.unir.roleapp.adventure.ui.screens.CreateAdventureScreen
+import com.unir.roleapp.adventure.ui.screens.WaitingRoomScreen
+import com.unir.roleapp.adventure.ui.viewmodels.WaitingRoomViewModel
+import com.unir.roleapp.auth.ui.screens.LoginScreen
+import com.unir.roleapp.auth.ui.screens.UserProfileScreen
+import com.unir.roleapp.auth.viewmodels.AuthViewModel
+import com.unir.roleapp.character.ui.screens.characterSheet.CharacterListScreen
+import com.unir.roleapp.character.ui.screens.characterSheet.CharacterDetailScreen
+import com.unir.roleapp.character.ui.screens.characterform.CharacterEditorScreen
+import com.unir.roleapp.core.di.LocalAuthViewModel
+import com.unir.roleapp.core.di.LocalLanguageSetter
+import com.unir.roleapp.core.di.LocalNavigationViewModel
 
 @Composable
 fun NavGraph(
@@ -93,16 +107,74 @@ fun NavGraph(
                 }
 
 
-                // ADVENTURE
-                composable(ScreensRoutes.HomeAdventureScreen.route) {
-                    HomeAdventureScreen(navController = navController)
+                // ---- AVENTURA ----
+
+                // 1) Pantalla de creación inicial
+                composable(ScreensRoutes.AdventureMainScreen.route) {
+                    AdventureMainScreen(navController = navController)
                 }
-                composable(ScreensRoutes.TemplateAdventureScreen.route) {
-                    TemplateAdventureScreen()
+
+                // 2) Sala de espera
+                composable(
+                    route = ScreensRoutes.WaitingRoomScreen.route,
+                    arguments = listOf(navArgument("adventureId") {
+                        type = NavType.StringType
+                    })
+                ) { backStack ->
+                    val adventureId = backStack.arguments!!.getString("adventureId")!!
+
+                    val vm: WaitingRoomViewModel = hiltViewModel()
+                    LaunchedEffect(adventureId) { vm.loadCharacters(adventureId) }
+
+                    val characters by vm.characters.collectAsState(initial = emptyList())
+                    val loading    by vm.loading.collectAsState(initial = false)
+                    val error      by vm.error.collectAsState(initial = null)
+
+                    WaitingRoomScreen(
+                        adventureId = adventureId,
+                        characters  = characters,
+                        loading     = loading,
+                        error       = error,
+                        onContinue  = {
+                            navController.navigate(
+                                ScreensRoutes.CreateAdventureScreen.createRoute(adventureId)
+                            )
+                        }
+                    )
                 }
 
+                // 3) Ajustes finales antes del contexto
+                composable(
+                    route     = ScreensRoutes.CreateAdventureScreen.route,
+                    arguments = listOf(navArgument("adventureId") {
+                        type = NavType.StringType
+                    })
+                ) { backStack ->
+                    val adventureId = backStack.arguments!!.getString("adventureId")!!
+                    CreateAdventureScreen(
+                        navController = navController,
+                        onNext        = { id ->
+                            navController.navigate(
+                                ScreensRoutes.AdventureContextScreen.createRoute(id)
+                            )
+                        }
+                    )
+                }
 
-
+                // 4) Contexto histórico y de personajes
+                composable(
+                    route     = ScreensRoutes.AdventureContextScreen.route,
+                    arguments = listOf(navArgument("adventureId") {
+                        type = NavType.StringType
+                    })
+                ) { backStack ->
+                    val adventureId = backStack.arguments!!.getString("adventureId")!!
+                    AdventureContextScreen(
+                        adventureId = adventureId,
+                        onCancel    = { navController.popBackStack() },
+                        onFinish    = { navController.popBackStack() }
+                    )
+                }
 
                 // HOME
                 composable(ScreensRoutes.HomeScreen.route) {
